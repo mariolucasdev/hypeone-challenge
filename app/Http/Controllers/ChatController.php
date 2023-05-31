9cf939aedd3dc68f563199cd6dca87eaa250b0e4
Mário Lucas <mariolucasdev@gmail.com>
2023-05-30 21:32:48 -0300

    Add Close Chat Action API

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,20 +4,53 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ChatStoreRequest;
 use App\Models\Chat;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    public function start(ChatStoreRequest $request)
+    /**
+     * Start Chat Session
+     *
+     * @param ChatStoreRequest $request
+     * @return JsonResponse
+     */
+    public function start(ChatStoreRequest $request): JsonResponse
     {
         $validated = $request->validated();
 
         if ($validated) {
+
             $chat = Chat::create($request->all());
 
             return response()->json($chat, 201);
         }
 
         return response()->json(['error' => 'Não foi possível iniciar o atendimento.'], 500);
+    }
+
+    /**
+     * Close Chat Session
+     *
+     * @param string $id
+     * @return JsonResponse
+     */
+    public function close(string $id): JsonResponse
+    {
+        $chat = Chat::find($id);
+
+        $closed = $chat->update(['closed' => true]);
+
+        if ($closed) {
+            return response()->json($chat, 200);
+        }
+
+        return response()->json(
+            [
+                'error' => 'Não foi possível finalizar o atendimento.',
+                'chat' => $chat
+            ],
+            500
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/chat', [ChatController::class, 'start']);
+
+Route::put('/chat/{id}/close', [ChatController::class, 'close']);
